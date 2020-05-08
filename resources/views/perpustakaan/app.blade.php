0d1a010fa9e420e919a5e6f2b4484619a73683ba
Neddy <neddy1298@gmail.com>
2020-05-08 20:39:10 +0700

    Change asset to secure_asset

--- resources/views/perpustakaan/app.blade.php
+++ resources/views/perpustakaan/app.blade.php
@@ -27,15 +27,15 @@
             CSS
             ============================================= -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="{{ asset('perpus/css/linearicons.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/nice-select.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/animate.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/owl.carousel.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/main.css')}}">
-    <link rel="stylesheet" href="{{ asset('perpus/css/main.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/linearicons.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/main.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('perpus/css/main.css')}}">
 </head>
 
 <body>
@@ -84,28 +84,28 @@
 
 @include('perpustakaan.navbar.footer')
 
-<script src="{{ asset('perpus/js/vendor/jquery-2.2.4.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/vendor/jquery-2.2.4.min.js')}}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
 </script>
-<script src="{{ asset('perpus/js/vendor/bootstrap.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/vendor/bootstrap.min.js')}}"></script>
 <script type="text/javascript"
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-<script src="{{ asset('perpus/js/easing.min.js')}}"></script>
-<script src="{{ asset('perpus/js/hoverIntent.js')}}"></script>
-<script src="{{ asset('perpus/js/superfish.min.js')}}"></script>
-<script src="{{ asset('perpus/js/jquery.ajaxchimp.min.js')}}"></script>
-<script src="{{ asset('perpus/js/jquery.magnific-popup.min.js')}}"></script>
-<script src="{{ asset('perpus/js/owl.carousel.min.js')}}"></script>
-<script src="{{ asset('perpus/js/owl-carousel-thumb.min.js')}}"></script>
-<script src="{{ asset('perpus/js/jquery.sticky.js')}}"></script>
-<script src="{{ asset('perpus/js/jquery.nice-select.min.js')}}"></script>
-<script src="{{ asset('perpus/js/parallax.min.js')}}"></script>
-<script src="{{ asset('perpus/js/waypoints.min.js')}}"></script>
-<script src="{{ asset('perpus/js/wow.min.js')}}"></script>
-<script src="{{ asset('perpus/js/jquery.counterup.min.js')}}"></script>
-<script src="{{ asset('perpus/js/mail-script.js')}}"></script>
-<script src="{{ asset('perpus/js/main.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/easing.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/hoverIntent.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/superfish.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/jquery.ajaxchimp.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/owl.carousel.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/owl-carousel-thumb.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/jquery.sticky.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/jquery.nice-select.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/parallax.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/waypoints.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/wow.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/jquery.counterup.min.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/mail-script.js')}}"></script>
+<script src="{{ secure_asset('perpus/js/main.js')}}"></script>
 </body>
 <script>
     var owl = $('.owl-carousel');
